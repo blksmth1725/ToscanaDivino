3a798140cd335d46ab3556020bb95a75fc7de97f
Christian Sheen <christiansheen@Christians-MacBook-Pro.local>
2020-07-03 01:13:06 -0400

    Styled header, changed name. added a new property to the CategoryScreen component (navigationOptions) so that header title could be hardcoded and headerstyles could be assigned. int the styling I made certain styles platform specific with the Platform API provided by react-native. ALSO: moved renderGridItem function into scope of Component for performance reasons

---
+++ constant/Colors.js
@@ -0,0 +1,6 @@
+export default {
+	pastelYellowColor: "#9bbbfc",
+	liteVioletColor: "#f9ce69",
+	litestBlueColor: "#d9eff7",
+	darkVioletColor: "#4741a6",
+};
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,17 +1,32 @@
 import React from "react";
-import { View, Text, StyleSheet, Button, FlatList } from "react-native";
+import {
+	View,
+	Text,
+	StyleSheet,
+	TouchableOpacity,
+	FlatList,
+	Platform,
+} from "react-native";
 
 import { CATEGORIES } from "../data/dummy-data";
-
-const renderGridItem = (itemData) => {
-	return (
-		<View style={styles.gridItem}>
-			<Text>{itemData.item.title}</Text>
-		</View>
-	);
-};
+import Colors from "../constant/Colors";
 
 const CategoriesScreen = (props) => {
+	const renderGridItem = (itemData) => {
+		return (
+			<TouchableOpacity
+				style={styles.gridItem}
+				onPress={() => {
+					props.navigation.navigate({ routeName: "CategoryMeals" });
+				}}
+			>
+				<View>
+					<Text>{itemData.item.title}</Text>
+				</View>
+			</TouchableOpacity>
+		);
+	};
+
 	return (
 		<FlatList
 			keyExtractor={(item, index) => item.id}
@@ -20,6 +35,16 @@ const CategoriesScreen = (props) => {
 			numColumns={2}
 		/>
 	);
+};
+
+CategoriesScreen.navigationOptions = {
+	headerTitle: "Meal Categories",
+	headerStyle: {
+		backgroundColor:
+			Platform.OS === "android" ? Colors.darkVioletColor : "",
+	},
+	headerTintColor:
+		Platform.OS === "android" ? "white" : Colors.darkVioletColor,
 };
 
 const styles = StyleSheet.create({
